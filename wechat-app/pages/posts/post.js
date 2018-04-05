fbf488f6a09a8aba87c1c74ccd9cdf05e19f46bf
cupeng <872345732@qq.com>
2018-04-05 22:19:27 +0800

    Small project

---
+++ wechat-app/pages/posts/post.js
@@ -0,0 +1,27 @@
+Page({
+    data: {
+        date:"Sep 18 2016",
+        title: "正式虾肥蟹壮时1"
+    },
+    imgPath: "/images/...",
+    process: function() {
+        var date = "Now 18 2016"
+        var ele = document.getElementById('id')
+    },
+    onLoad: function() {
+        //页面初始化
+    },
+    onReady: function() {
+        //页面渲染完成
+
+    },
+    onShow: function() {
+        //页面显示
+    },
+    onHide: function() {
+        //页面隐藏
+    },
+    onUnload: function() {
+        //页面关闭
+    }
+})
